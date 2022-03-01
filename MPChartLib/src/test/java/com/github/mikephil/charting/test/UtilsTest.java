b934b6a901228a1062b669c2633da6ad192dca0e
Lin <liulinzhi001@gmail.com>
2022-03-01 14:33:54 -0800

    split test cases

--- MPChartLib/src/test/java/com/github/mikephil/charting/test/UtilsTest.java
+++ MPChartLib/src/test/java/com/github/mikephil/charting/test/UtilsTest.java
@@ -8,23 +8,38 @@ import org.junit.Test;
 
 public class UtilsTest {
     @Test
-    public void formatNumberTest() {
+    public void formatNumberTestOnZero() {
         String number = Utils.formatNumber(0.0f, 0, true);
         assertEquals("0", number);
+    }
 
-        number = Utils.formatNumber(0.01f, 2, true);
+    @Test
+    public void formatNumberTestOnLessThanOne() {
+        String number = Utils.formatNumber(0.01f, 2, true);
         assertEquals("0,01", number);
+    }
 
-        number = Utils.formatNumber(2f, 11, true);
+    @Test
+    public void formatNumberTestOnTooMuchDecimal() {
+        String number = Utils.formatNumber(2f, 11, true);
         assertEquals("2,000000000", number);
+    }
 
-        number = Utils.formatNumber(-2f, 0, true);
+    @Test
+    public void formatNumberTestOnNegative() {
+        String number = Utils.formatNumber(-2f, 0, true);
         assertEquals("-2", number);
+    }
 
-        number = Utils.formatNumber(22367670.004f, 0, true);
-        assertEquals("22.367.670", number);
-
-        number = Utils.formatNumber(2000f, 2, true);
+    @Test
+    public void formatNumberTestOnNumberOverThousand() {
+        String number = Utils.formatNumber(2000f, 2, true);
         assertEquals("2.000,00", number);
+    }
+
+    @Test
+    public void formatNumberTestOnNumberOverThousand2() {
+        String number = Utils.formatNumber(22367670.004f, 0, true);
+        assertEquals("22.367.670", number);
     }
 }
