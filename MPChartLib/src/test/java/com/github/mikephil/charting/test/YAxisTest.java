afa6ccd9afe1d1e2ec24d87dff338f83aa3c67f8
Justin Grant <justingrant99@me.com>
2022-03-01 14:39:40 -0800

    YAxisCalculate Split test Cases

--- MPChartLib/src/test/java/com/github/mikephil/charting/test/YAxisTest.java
+++ MPChartLib/src/test/java/com/github/mikephil/charting/test/YAxisTest.java
@@ -31,49 +31,65 @@ public class YAxisTest {
 
     }
 
-    @Test
-    public void YAxisCalculate() {
-        YAxis yAxis = new YAxis();
-        YAxis yAxis2 = new YAxis();
 
+    @Test
+    public void YAxisCalculateRangeIsZero(){
+        YAxis yAxis = new YAxis();
         // range zero
         yAxis.calculate(0, 0);
         Float axisMinimum = yAxis.getAxisMinimum();
         Float axisMaximum = yAxis.getAxisMaximum();
         Float range = Math.abs(axisMinimum - axisMaximum);
         assertEquals(2.4f, range);
+    }
 
+    @Test
+    public void YAxisCalculateAxisCustomMinimumandMaximumAreFalse(){
+        YAxis yAxis = new YAxis();
         // neither are true
         yAxis.calculate(10, 2);
-        axisMinimum = yAxis.getAxisMinimum();
-        axisMaximum = yAxis.getAxisMaximum();
-        range = Math.abs(axisMinimum - axisMaximum);
+        Float axisMinimum = yAxis.getAxisMinimum();
+        Float axisMaximum = yAxis.getAxisMaximum();
+        Float range = Math.abs(axisMinimum - axisMaximum);
         assertEquals(6.3999996f, range);
+    }
 
+    @Test
+    public void YAxisCalculateCustomAxisMinimumTrue(){
+        YAxis yAxis = new YAxis();
         // CustomAxisMinmum is true
         yAxis.setAxisMinimum(10f);
         yAxis.calculate(10, 2);
 
-        axisMinimum = yAxis.getAxisMinimum();
-        axisMaximum = yAxis.getAxisMaximum();
-        range = Math.abs(axisMinimum - axisMaximum);
+        Float axisMinimum = yAxis.getAxisMinimum();
+        Float axisMaximum = yAxis.getAxisMaximum();
+        Float range = Math.abs(axisMinimum - axisMaximum);
         assertEquals(5.5f, range);
+    }
 
-        // both CustomaxisMinimum and CustomaxisMaximum are true
-        yAxis.setAxisMaximum(2f);
-        yAxis.calculate(10, 2);
-        axisMinimum = yAxis.getAxisMinimum();
-        axisMaximum = yAxis.getAxisMaximum();
-        range = Math.abs(axisMinimum - axisMaximum);
-        assertEquals(8f, range);
-
+    @Test
+    public void YAxisCalculateCustomAxisMaximumTrue(){
+        YAxis yAxis = new YAxis();
         // only CustomaxisMaximum is true
-        yAxis2.setAxisMaximum(11f);
-        yAxis2.calculate(10, -2);
-        Float axisMinimum2 = yAxis2.getAxisMinimum();
-        Float axisMaximum2 = yAxis2.getAxisMaximum();
+        yAxis.setAxisMaximum(11f);
+        yAxis.calculate(10, -2);
+        Float axisMinimum2 = yAxis.getAxisMinimum();
+        Float axisMaximum2 = yAxis.getAxisMaximum();
         Float range2 = Math.abs(axisMinimum2 - axisMaximum2);
         assertEquals(14.1f, range2);
+    }
+
+    @Test
+    public void YAxisCalculateCustomAxisMaximumAndMinimumTrue(){
+        YAxis yAxis = new YAxis();
+        // both CustomaxisMinimum and CustomaxisMaximum are true
+        yAxis.setAxisMinimum(10f);
+        yAxis.setAxisMaximum(2f);
+        yAxis.calculate(10, 2);
+        Float axisMinimum = yAxis.getAxisMinimum();
+        Float axisMaximum = yAxis.getAxisMaximum();
+        Float range = Math.abs(axisMinimum - axisMaximum);
+        assertEquals(8f, range);
 
     }
 
